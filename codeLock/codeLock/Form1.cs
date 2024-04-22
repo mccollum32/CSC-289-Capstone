9b0e0b4ef07639155aaef1dd0360563d80d06ac1
Joseph McCollum <103016879+mccollum32@users.noreply.github.com>
2024-04-22 14:41:43 -0400

    Add files via upload
    Code Lock is a password manager app run in C# was previously in C++ but QT creator wasn't running properly for some reason.

---
+++ codeLock/codeLock/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+//Main app code page
+
+
+namespace codeLock
+{
+    public partial class mainPG : Form
+    {
+        //This sets the background color of the App to Wood color
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int LPAR);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        const int WM_NCLBUTTONDOWN = 0xA1;
+        const int HT_CAPTION = 0x2;
+
+
+        public mainPG()
+        {
+            InitializeComponent();
+            this.MouseDown += new MouseEventHandler(move_window);
+
+        }
+
+
+        private void move_window(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
